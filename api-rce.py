c4e5538c40e671c0018cbcb917815aa9897cecb5
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-03-21 17:21:18 -0400

    API RCE

---
+++ api-rce.py
@@ -0,0 +1,32 @@
+"""
+Author: Aleksa Zatezalo
+Description: Obtains RCE on Concord servers through RCE.
+Date: March 2024
+"""
+
+
+def printLog(message):
+    """
+    Logs the string, message, to console with the appendage '[+] '.
+    """
+
+    pass
+
+def constructYaml(lhost, lport):
+    """
+    Constructs a YAML file containing a workflow and RCE where lhost and lport are the
+    ip and ports of the listener for an interactive shell found at lhost:lport.
+    """
+    
+    pass
+
+def obtainRCE(yaml, rhost):
+    """
+    Takes a yaml file containing an infected process and obtains RCE on the Concord
+    remote host, rhost, by creating an infected workflow.
+    """
+
+    pass
+
+if __name__ == "__main__":
+    pass
